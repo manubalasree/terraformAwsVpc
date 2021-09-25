bcce6eee14e4db035886a3a0f4c130fe1a783e5f
Manu Balasree <manubalasree@gmail.com>
2021-09-26 02:08:25 +0530

    bugs

--- output.tf
+++ output.tf
@@ -4,7 +4,3 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = concat(aws_vpc.this.*.id, [""])[0]
 }
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = aws_subnet.public.*.id
-}
