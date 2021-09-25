9b7eb1a6197667d1791b3d60309905ed0a1a9f75
Manu Balasree <manubalasree@gmail.com>
2021-09-26 01:58:57 +0530

    bugs

--- backend.tf
+++ backend.tf
@@ -1,5 +1,12 @@
 # create backedn confoguration https://www.padok.fr/en/blog/terraform-s3-bucket-aws
 # sticking to partial config expect vars to be passed on init
+/*
+terraform init -backend-config="<buckey name>" \
+-backend-config="key=terraformawsvpc/terraform.tfstate" \
+-backend-config="region=us-east-2" \
+-backend-config="encrypt=true" \
+-backend-config="kms_key_id=<key id>"
+*/
 terraform {
   backend "s3" {}
 }
