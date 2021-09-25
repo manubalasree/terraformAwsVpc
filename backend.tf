96ad240190d0872465c13c4a510921c123ad236b
Manu Balasree <manubalasree@gmail.com>
2021-09-26 00:59:37 +0530

    bugs

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "tfs3backend-639232547460Info"
+    bucket         = "tfs3backend-639232547460"
     key            = "terraformawsvpc/terraform.tfstate"
     region         = "us-east-2"
     encrypt        =  true
