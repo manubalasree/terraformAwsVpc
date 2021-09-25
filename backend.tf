59302ae3fbc287b3f6365ead866a80b9bb17e91b
Manu Balasree <manubalasree@gmail.com>
2021-09-26 00:58:37 +0530

    bugs

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# create backedn confoguration https://www.padok.fr/en/blog/terraform-s3-bucket-aws
+
+terraform {
+  backend "s3" {
+    bucket         = "tfs3backend-639232547460Info"
+    key            = "terraformawsvpc/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        =  true
+    kms_key_id     = "19f4ef43-74a8-4d23-82e3-2e4f2936a948"
+  }
+}
