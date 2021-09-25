525911a1d772fb4f53e54e695679bcdba1708993
Manu Balasree <manubalasree@gmail.com>
2021-09-26 02:07:35 +0530

    bugs

--- output.tf
+++ output.tf
@@ -1,11 +1,10 @@
 # Output variable definitions
 
-output "vpc_public_subnets" {
-  description = "IDs of the VPC's public subnets"
-  value       = module.vpc.public_subnets
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = concat(aws_vpc.this.*.id, [""])[0]
 }
-
-output "ec2_instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
-  value       = module.ec2_instances.public_ip
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public.*.id
 }
